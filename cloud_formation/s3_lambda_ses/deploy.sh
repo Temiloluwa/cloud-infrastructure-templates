d4e927387132cc42a96fda9d87daf882b1ed4f83
Temiloluwa <temilolu74@yahoo.co.uk>
2022-06-19 20:11:10 +0200

    modified delopy.sh

--- cloud_formation/s3_lambda_ses/deploy.sh
+++ cloud_formation/s3_lambda_ses/deploy.sh
@@ -1,10 +1,35 @@
 #!/bin/sh
-aws cloudformation package \
-    --template-file template.yaml \
-    --s3-bucket cf-templates-1sj9kzph2p7xj-us-east-1 \
-    --output-template-file template-packaged.yaml
+while getopts t:b:o:s:r: flag
+do
+    case "${flag}" in
+        t) template=${OPTARG};;
+        b) bucket=${OPTARG};;
+        o) output=${OPTARG};;
+        s) stackname=${OPTARG};;
+        r) region=${OPTARG};;
+    esac
+done
 
+echo "region: $region";
+echo "cloudformation template: $template";
+echo "cloudformation template s3 bucket name: $bucket";
+echo "cloudformation output template: $output";
+echo "cloudformation stack name: $stackname";
+
+# create s3 bucket to deploy cloud formation template
+aws s3 mb "s3://$bucket" --region $region
+
+# install python dependencies
+pip install -r lambda-notify/requirements.txt --target lambda-notify
+
+# package template
+aws cloudformation package \
+    --template-file $template \
+    --s3-bucket $bucket\
+    --output-template-file $output
+
+# deploy template
 aws cloudformation deploy \
     --capabilities CAPABILITY_IAM \
-    --template-file /Users/t.adeoti/Documents/aws/cloudformation/s3-notifications/final/s3_lambda_ses/template-packaged.yaml \
-    --stack-name ses-s3-notification-stack-0070
+    --template-file $output \
+    --stack-name $stackname
